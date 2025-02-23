52f97ff021ad9d56274ccbde8282eecdd4bc6292
inahym196 <subihym@gmail.com>
2025-02-24 01:43:17 +0900

    Interactor作成によりControllerがdomainを直接参照しなくなった

--- internal/controller/cli_controller.go
+++ internal/controller/cli_controller.go
@@ -6,52 +6,15 @@ import (
 	"os"
 
 	"github.com/MakeNowJust/heredoc"
-	"github.com/inahym196/bomb/internal/domain"
+	"github.com/inahym196/bomb/internal/interactor"
 )
 
 type CLIController struct {
-	game *domain.Game
+	gi *interactor.GameInteractor
 }
 
-func NewCLIController(game *domain.Game) *CLIController {
-	return &CLIController{game}
-}
-
-func (c *CLIController) getBoard() string {
-	var output string
-	cells := c.game.GetBoard().GetCells()
-	cellsDTO := cellsToStr(cells)
-	for _, row := range cellsDTO {
-		for _, cell := range row {
-			output += fmt.Sprintf(" %s", cell)
-		}
-		output += "\n"
-	}
-	return output
-}
-
-func cellsToStr(cells [][]domain.Cell) [][]string {
-	dto := make([][]string, len(cells))
-	for i, row := range cells {
-		dto[i] = make([]string, len(cells))
-		for j, cell := range row {
-			dto[i][j] = cellToStr(cell)
-		}
-	}
-	return dto
-}
-
-func cellToStr(cell domain.Cell) string {
-	switch cell.GetState() {
-	case domain.CellBomb:
-		return "B"
-	case domain.CellOpen:
-		return " "
-	case domain.CellClosed:
-		return "?"
-	default:
-		return ""
-	}
+func NewCLIController(gi *interactor.GameInteractor) *CLIController {
+	return &CLIController{gi}
 }
 
 func (c *CLIController) Run() {
@@ -78,5 +41,29 @@ func (c *CLIController) Run() {
 			fmt.Printf("\"%s\" is invalid command. Use \"help\"\n", input.Text())
 		}
 	}
+}
 
+func (c *CLIController) getBoard() string {
+	var output string
+	states := c.gi.GetBoardCellStates().CellStates
+	for _, row := range states {
+		for _, state := range row {
+			output += fmt.Sprintf(" %s", stateToStr(state))
+		}
+		output += "\n"
+	}
+	return output
+}
+
+func stateToStr(state byte) string {
+	switch state {
+	case interactor.CellStateBomb:
+		return "B"
+	case interactor.CellStateOpen:
+		return " "
+	case interactor.CellStateClosed:
+		return "?"
+	default:
+		return ""
+	}
 }
---
+++ internal/interactor/game_dto.go
@@ -0,0 +1,40 @@
+package interactor
+
+import (
+	"github.com/inahym196/bomb/internal/domain"
+)
+
+const (
+	CellStateUndefined byte = iota
+	CellStateClosed
+	CellStateBomb
+	CellStateOpen
+)
+
+func CellStatesFrom(cells [][]domain.Cell) [][]byte {
+	bytes := make([][]byte, len(cells))
+	for i, row := range cells {
+		bytes[i] = make([]byte, len(row))
+		for j, cell := range row {
+			bytes[i][j] = CellStateFrom(cell)
+		}
+	}
+	return bytes
+}
+
+func CellStateFrom(cell domain.Cell) byte {
+	switch cell.GetState() {
+	case domain.CellClosed:
+		return CellStateClosed
+	case domain.CellBomb:
+		return CellStateBomb
+	case domain.CellOpen:
+		return CellStateClosed
+	default:
+		return CellStateUndefined
+	}
+}
+
+type GetBoardCellStatesOutput struct {
+	CellStates [][]byte
+}
