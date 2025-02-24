7e755b5e222eeb988ad1470a4e8d2ebb65d0a142
USER <user@USERnoMacBook-Pro.local>
2025-02-25 03:29:11 +0900

    Gameの初期化を行えるようにした

--- internal/interactor/game_interactor.go
+++ internal/interactor/game_interactor.go
@@ -1,24 +1,58 @@
 package interactor
 
-import "github.com/inahym196/bomb/internal/domain"
+import (
+	"fmt"
 
-type GameInteractor struct {
-	game *domain.Game
-}
+	"github.com/inahym196/bomb/internal/domain"
+)
+
+var ImmemoryGame *domain.Game
+
+type GameInteractor struct{}
 
 func NewGameInteractor() *GameInteractor {
+	return &GameInteractor{}
+}
+
+type Position struct {
+	Row int
+	Col int
+}
+
+type InitGameParam struct {
+	BoardWidth int
+	Bombs      []Position
+}
+type InitGameResult struct {
+	GameDTO
+}
+
+func (gi *GameInteractor) InitGame(param InitGameParam) *InitGameResult {
 	opt := &domain.GameOption{
-		BoardWidth: 8,
-		Bombs:      []domain.Position{{Row: 0, Col: 0}, {Row: 1, Col: 2}},
+		BoardWidth: param.BoardWidth,
+		Bombs:      toPositions(param.Bombs),
 	}
-	return &GameInteractor{domain.NewGame(opt)}
+	ImmemoryGame = domain.NewGame(opt)
+	dto := toGameDTO(ImmemoryGame)
+	return &InitGameResult{dto}
+}
+
+func toPositions(dto []Position) []domain.Position {
+	poss := make([]domain.Position, len(dto))
+	for i, pos := range dto {
+		poss[i] = domain.Position{Row: pos.Row, Col: pos.Col}
+	}
+	return poss
 }
 
 type GetGameResult struct {
 	GameDTO
 }
 
-func (gi *GameInteractor) GetGame() GetGameResult {
-	dto := toGameDTO(gi.game)
-	return GetGameResult{dto}
+func (gi *GameInteractor) GetGame() (GetGameResult, error) {
+	game := ImmemoryGame
+	if game == nil {
+		return GetGameResult{}, fmt.Errorf("ゲームが初期化されていません")
+	}
+	return GetGameResult{toGameDTO(game)}, nil
 }
