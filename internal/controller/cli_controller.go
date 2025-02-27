3cb82b02061a3ad2cc621c3561f1675f8ef0fa5f
USER <user@USERnoMacBook-Pro.local>
2025-02-28 01:50:05 +0900

    エラーメッセージを日本語にした

--- internal/controller/cli_controller.go
+++ internal/controller/cli_controller.go
@@ -56,17 +56,17 @@ func (c *CLIController) Run() {
 			fmt.Print(c.debugGame(result.GameDTO))
 		case "open":
 			if wordLen != 3 {
-				fmt.Println("invalid num of args. See \"help\"")
+				fmt.Println("引数の数が不正です. \"help\"コマンドを確認してください")
 				continue
 			}
 			row, err := strconv.Atoi(words[1])
 			if err != nil {
-				fmt.Println("invalid row. row should be a number.")
+				fmt.Println("rowの値が不正です. 数字を入力してください")
 				continue
 			}
 			col, err := strconv.Atoi(words[2])
 			if err != nil {
-				fmt.Println("invalid column. column should be a number.")
+				fmt.Println("columnの値が不正です. 数字を入力してください")
 				continue
 			}
 			result, err := c.gi.OpenCell(interactor.OpenCellParam{Row: row, Col: col})
@@ -76,7 +76,7 @@ func (c *CLIController) Run() {
 			}
 			fmt.Print(c.parseGame(result.GameDTO))
 		case "exit", "quit", "q":
-			fmt.Println("Exiting...")
+			fmt.Println("終了中...")
 			return
 		case "help", "h":
 			fmt.Print(heredoc.Doc(`
@@ -88,7 +88,7 @@ func (c *CLIController) Run() {
 			  > exit              Exit the program
 			`))
 		default:
-			fmt.Printf("\"%s\" is invalid command. See \"help\"\n", text)
+			fmt.Printf("\"%s\"は無効なコマンドです. \"help\"コマンドを確認してください\n", text)
 		}
 		fmt.Println()
 	}
