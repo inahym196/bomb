734c6e4a4f86f470d3d1308dc5df64eea3603253
inahym196 <subihym@gmail.com>
2025-03-03 00:38:24 +0900

    refactor: domain.Cellを別ファイルに分割した

--- internal/domain/board.go
+++ internal/domain/board.go
@@ -6,41 +6,6 @@ import (
 	"github.com/inahym196/bomb/pkg/shared"
 )
 
-type Cell struct {
-	isOpened  bool
-	isBomb    bool
-	bombCount int
-}
-
-func (c Cell) IsOpened() bool    { return c.isOpened }
-func (c Cell) IsBomb() bool      { return c.isBomb }
-func (c Cell) GetBombCount() int { return c.bombCount }
-
-func NewCell(isBomb bool) Cell {
-	if isBomb {
-		return Cell{false, true, -1}
-	}
-	return Cell{false, false, 0}
-}
-
-func (c Cell) Open() (Cell, error) {
-	if c.isOpened {
-		return Cell{}, fmt.Errorf("すでに開放済みのセルです")
-	}
-	return Cell{isOpened: true, isBomb: c.isBomb, bombCount: c.bombCount}, nil
-}
-
-func (c Cell) IncrementBombCount() (Cell, error) {
-	if c.bombCount == -1 {
-		return Cell{}, fmt.Errorf("ボムのマスは計算対象外です")
-	}
-	return Cell{
-		isOpened:  c.isOpened,
-		isBomb:    c.isBomb,
-		bombCount: c.bombCount + 1,
-	}, nil
-}
-
 type Board struct {
 	width           int
 	cells           [][]Cell
