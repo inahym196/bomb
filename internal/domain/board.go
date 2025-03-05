02067df76a3dd9ee8c700e23493fceb621572f58
inahym196 <subihym@gmail.com>
2025-03-06 02:09:57 +0900

    refactor: GameからBoardWidthを削除

--- internal/domain/board.go
+++ internal/domain/board.go
@@ -14,6 +14,7 @@ type Board struct {
 }
 
 func (b *Board) GetCells() [][]Cell                     { return b.cells }
+func (b *Board) GetWidth() int                          { return b.width }
 func (b *Board) MustGetCellAt(pos shared.Position) Cell { return b.cells[pos.Y][pos.X] }
 func (b *Board) GetClosedCellCount() (count int)        { return b.closedCellCount }
 func (b *Board) GetCellAt(pos shared.Position) (Cell, error) {
