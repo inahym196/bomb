5599f37f1aab5db021beb86292ecefad6709819e
USER <user@USERnoMacBook-Pro.local>
2025-03-06 02:55:57 +0900

    update: CellをOpened/Closedに分けた

--- internal/solver/domain/cell.go
+++ internal/solver/domain/cell.go
@@ -5,59 +5,60 @@ import (
 	"github.com/inahym196/bomb/pkg/shared"
 )
 
-// TODO: Impl bombScope
-//type bombScope struct {
-//	origin      *Cell
-//	closedCells []Cell
-//}
-//
-//func NewBombScope(cell *Cell) *bombScope {
-//	return &bombScope{cell, []Cell{}}
-//}
-
-type Cell struct {
-	isSolved  bool
-	isClosed  bool
-	bombCount int
-	// TODO: bombScope *bombScope
-	// TODO: scopedBy []bombScope
+type bombScope struct {
+	origin      *OpenCell
+	closedCells []ClosedCell
 }
 
-func (c Cell) IsSolved() bool    { return c.isSolved }
-func (c Cell) IsClosed() bool    { return c.isClosed }
-func (c Cell) GetBombCount() int { return c.bombCount }
+func NewBombScope(*OpenCell) *bombScope {
+	// TODO
+	return &bombScope{nil, []ClosedCell{}}
+}
 
-func ToSolverCells(cells [][]interactor.CellDTO) map[shared.Position]Cell {
-	scells := make(map[shared.Position]Cell)
+type ClosedCell struct {
+	scopedBy []bombScope
+}
+
+type OpenCell struct {
+	bombScope *bombScope
+	//isSolved  bool
+	bombCount int
+}
+
+func ToSolverCells(cells [][]interactor.CellDTO) (map[shared.Position]OpenCell, map[shared.Position]ClosedCell) {
+	openCells := make(map[shared.Position]OpenCell)
+	closedCells := make(map[shared.Position]ClosedCell)
 	for i := range cells {
 		for j, cell := range cells[i] {
 			pos := shared.NewPosition(j, i)
-			if cell.IsOpened && cell.BombCount == 0 {
+			if !cell.IsOpened {
+				closedCells[pos] = ClosedCell{nil}
 				continue
 			}
-			scells[pos] = NewCell(cells, pos)
+			if cell.BombCount != 0 {
+				openCells[pos] = NewOpenCell(cells, pos)
+			}
 		}
 	}
-	return scells
+	return openCells, closedCells
 }
 
-func NewCell(cells [][]interactor.CellDTO, pos shared.Position) Cell {
-	if !cells[pos.Y][pos.X].IsOpened {
-		return Cell{false, true, -1}
-	}
-	return Cell{isSolvedCell(cells, pos), false, cells[pos.Y][pos.X].BombCount}
+func NewOpenCell(cells [][]interactor.CellDTO, pos shared.Position) OpenCell {
+	return OpenCell{nil, cells[pos.Y][pos.X].BombCount}
 }
 
-func isSolvedCell(cells [][]interactor.CellDTO, pos shared.Position) bool {
-	// 周りのセルのうち、Cells内に含まれているセルが全てOpenの場合はSolved
-	return pos.ForEachDirectionSatisfy(func(pos shared.Position) (ok bool) {
-		// posがCells内に含まれない場合はSolveに関係ないためok判定
-		return !isInCells(cells, pos) || cells[pos.Y][pos.X].IsOpened
-	})
-}
-
-func isInCells(cells [][]interactor.CellDTO, pos shared.Position) bool {
-	min := 0
-	max := len(cells)
-	return min <= pos.X && pos.X < max && min <= pos.Y && pos.Y < max
-}
+// そもそもSolved以外(BombCount!=0)のCellを集めているので全てのOpenCellがマッチするのでは？
+// -> そのうちCell.Checkedとか実装したら活きてくる。実装してからコメントを外す
+//func isSolvedCell(cells [][]interactor.CellDTO, pos shared.Position) bool {
+//	// 周りのセルのうち、Cells内に含まれているセルが全てOpenの場合はSolved
+//	return pos.ForEachDirectionSatisfy(func(pos shared.Position) (ok bool) {
+//		// posがCells内に含まれない場合はSolveに関係ないためok判定
+//		return !isInCells(cells, pos) || cells[pos.Y][pos.X].IsOpened
+//	})
+//}
+//
+//func isInCells(cells [][]interactor.CellDTO, pos shared.Position) bool {
+//	min := 0
+//	max := len(cells)
+//	return min <= pos.X && pos.X < max && min <= pos.Y && pos.Y < max
+//}
