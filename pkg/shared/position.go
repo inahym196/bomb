785c7345e52f18cdfce7ecf837de76889d471d96
USER <user@USERnoMacBook-Pro.local>
2025-03-06 02:13:16 +0900

    feature/solver.Cellの仮作成

--- pkg/shared/position.go
+++ pkg/shared/position.go
@@ -17,12 +17,14 @@ func NewRandomPosition(maxN int) Position {
 	return Position{rand.Intn(maxN), rand.Intn(maxN)}
 }
 
+// TODO: var neighbors
 var Directions = []struct{ dx, dy int }{
 	{-1, -1}, {-1, 0}, {-1, 1},
 	{0, -1}, {0, 1},
 	{1, -1}, {1, 0}, {1, 1},
 }
 
+// TODO: func ForEachNeighbor
 func (p Position) ForEachDirection(fn func(pos Position)) {
 	for _, dir := range Directions {
 		neighbor := p.offset(dir.dx, dir.dy)
@@ -32,4 +34,14 @@ func (p Position) ForEachDirection(fn func(pos Position)) {
 
 func (p Position) offset(dx, dy int) Position {
 	return Position{p.X + dx, p.Y + dy}
+}
+
+func (p Position) ForEachDirectionSatisfy(fn func(pos Position) (ok bool)) bool {
+	for _, dir := range Directions {
+		neighbor := p.offset(dir.dx, dir.dy)
+		if !fn(neighbor) {
+			return false
+		}
+	}
+	return true
 }
