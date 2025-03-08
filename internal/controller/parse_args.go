df7d733d1b82b531831e31a6f6dc8df8e1e65d1c
inahym196 <subihym@gmail.com>
2025-03-08 22:14:33 +0900

    feature: gameModeを実装

--- internal/controller/parse_args.go
+++ internal/controller/parse_args.go
@@ -2,18 +2,49 @@ package controller
 
 import (
 	"fmt"
+	"log"
 	"strconv"
 )
 
-func (c *CLIController) parseStartArgs(words []string) (boardWidth, bombCount int, err error) {
-	if len(words) != 3 {
+const (
+	GameModeUndefined byte = iota
+	GameModeEasy
+	GameModeNormal
+	//GameModeHard
+	GameModeCustom
+)
+
+func (c *CLIController) parseStartGameModeArgs(words []string) (mode byte, err error) {
+	if len(words) < 2 {
+		return GameModeUndefined, fmt.Errorf("引数の数が不正です. \"help\"コマンドを確認してください")
+	}
+	switch words[1] {
+	case "easy":
+		return GameModeEasy, nil
+	case "normal":
+		return GameModeNormal, nil
+	// まだwidthとheightを個別に選択できないのでhard(30*16)は実装できない
+	//case "hard":
+	//	return GameModeHard, nil
+	case "custom":
+		return GameModeCustom, nil
+	default:
+		return GameModeUndefined, fmt.Errorf("start [easy, normal, custom] のいずれかを指定してください")
+	}
+}
+
+func (c *CLIController) parseStartCustomModeArgs(words []string) (boardWidth, bombCount int, err error) {
+	if words[1] != "custom" {
+		log.Fatalln("この関数はcustomModeでのみ呼び出し可能です")
+	}
+	if len(words) != 4 {
 		return 0, 0, fmt.Errorf("引数の数が不正です. \"help\"コマンドを確認してください")
 	}
-	boardWidth, err = strconv.Atoi(words[1])
+	boardWidth, err = strconv.Atoi(words[2])
 	if err != nil {
 		return 0, 0, fmt.Errorf("boardWidthの値が不正です. 数字を入力してください")
 	}
-	bombCount, err = strconv.Atoi(words[2])
+	bombCount, err = strconv.Atoi(words[3])
 	if err != nil {
 		return 0, 0, fmt.Errorf("bombCountの値が不正です. 数字を入力してください")
 	}
