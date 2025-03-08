d62689e2537da1a247769c8af601117d5f528198
inahym196 <subihym@gmail.com>
2025-03-08 14:18:56 +0900

    refactor: split parseArgs func

--- internal/controller/cli_controller.go
+++ internal/controller/cli_controller.go
@@ -4,7 +4,6 @@ import (
 	"bufio"
 	"fmt"
 	"os"
-	"strconv"
 	"strings"
 
 	"github.com/MakeNowJust/heredoc"
@@ -107,36 +106,6 @@ func (c *CLIController) Run() {
 			fmt.Printf("\"%s\"は無効なコマンドです. \"help\"コマンドを確認してください\n\n", text)
 		}
 	}
-}
-
-func (c *CLIController) parseStartArgs(words []string) (boardWidth, bombCount int, err error) {
-	if len(words) != 3 {
-		return 0, 0, fmt.Errorf("引数の数が不正です. \"help\"コマンドを確認してください")
-	}
-	boardWidth, err = strconv.Atoi(words[1])
-	if err != nil {
-		return 0, 0, fmt.Errorf("boardWidthの値が不正です. 数字を入力してください")
-	}
-	bombCount, err = strconv.Atoi(words[2])
-	if err != nil {
-		return 0, 0, fmt.Errorf("bombCountの値が不正です. 数字を入力してください")
-	}
-	return boardWidth, bombCount, nil
-}
-
-func (c *CLIController) parseOpenArgs(words []string) (row, col int, err error) {
-	if len(words) != 3 {
-		return 0, 0, fmt.Errorf("引数の数が不正です. \"help\"コマンドを確認してください")
-	}
-	row, err = strconv.Atoi(words[1])
-	if err != nil {
-		return 0, 0, fmt.Errorf("rowの値が不正です. 数字を入力してください")
-	}
-	col, err = strconv.Atoi(words[2])
-	if err != nil {
-		return 0, 0, fmt.Errorf("columnの値が不正です. 数字を入力してください")
-	}
-	return row, col, nil
 }
 
 func (c *CLIController) parseGame(game interactor.GameDTO) (output string) {
