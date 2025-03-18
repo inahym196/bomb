3e5a91e357f8325a63a15c8a4f4048778a654734
inahym196 <subihym@gmail.com>
2025-03-18 23:24:36 +0900

    refactor: theoremを複数設定できるようにした

--- internal/solver/domain/solver.go
+++ internal/solver/domain/solver.go
@@ -2,7 +2,6 @@ package domain
 
 import (
 	"fmt"
-	"maps"
 
 	"github.com/inahym196/bomb/pkg/shared"
 )
@@ -22,20 +21,25 @@ func NewSolver(cells map[shared.Position]OpenCell) solver {
 }
 
 func (s solver) Solve() string {
-	solution := s.theorems[0].Apply(s.cells)
-	if len(solution.Positions) == 0 {
-		return fmt.Sprintf("[%s]: 該当なし\n", s.theorems[0].GetDescription())
-	}
-	var result string
-	switch solution.Result {
-	case SolutionResultIsBomb:
-		result = "Bombである"
-	case SolutionResultIsNotBomb:
-		result = "開放できる"
-	}
-	str := fmt.Sprintf("[%s]に従い、\n以下は全て%s\n", s.theorems[0].GetDescription(), result)
-	for _, pos := range solution.Positions {
-		str += fmt.Sprintf("%v\n", pos)
+	var str string
+	for _, theorem := range s.theorems {
+		solution := theorem.Apply(s.cells)
+		if len(solution.Positions) == 0 {
+			str += fmt.Sprintf("[%s]: 該当なし\n", s.theorems[0].GetDescription())
+			continue
+		}
+		var result string
+		switch solution.Result {
+		case SolutionResultIsBomb:
+			result = "Bombである"
+		case SolutionResultIsNotBomb:
+			result = "開放できる"
+		}
+		str += fmt.Sprintf("[%s]に従い、\n以下は全て%s\n", theorem.GetDescription(), result)
+		for _, pos := range solution.Positions {
+			str += fmt.Sprintf("%v\n", pos)
+		}
+		str += fmt.Sprint()
 	}
 	return str
 }
@@ -63,7 +67,7 @@ func (t theorem1) GetDescription() string {
 
 func (t theorem1) Apply(cells map[shared.Position]OpenCell) Solution {
 	poss := make([]shared.Position, 0, len(cells)/2)
-	for opencell := range maps.Values(cells) {
+	for _, opencell := range cells {
 		shadyCells := opencell.GetShadyCellKeys()
 		if len(shadyCells) <= opencell.GetBombCount() {
 			poss = append(poss, shadyCells...)
