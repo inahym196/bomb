bd91148b350cd5cc49dd8fc774e84b362ba80e6e
inahym196 <subihym@gmail.com>
2025-03-23 01:25:18 +0900

    refactor: InsideNeighborsを削除した

--- pkg/shared/position.go
+++ pkg/shared/position.go
@@ -54,14 +54,3 @@ func (p Position) Neighbors() []Position {
 	}
 	return nbs
 }
-
-func (p Position) InsideNeighbors(maxX, maxY int) []Position {
-	nbs := make([]Position, 0, 8)
-	for _, nb := range neighbors {
-		nb := p.offset(nb.dx, nb.dy)
-		if nb.IsInside(maxX, maxY) {
-			nbs = append(nbs, nb)
-		}
-	}
-	return nbs
-}
