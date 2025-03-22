9fe9c68e99858ab9ed09a7f8872ef410a673e17f
inahym196 <subihym@gmail.com>
2025-03-23 00:35:50 +0900

    feature/ForEachNeighbor to Neighbors

--- pkg/shared/position.go
+++ pkg/shared/position.go
@@ -43,13 +43,14 @@ var neighbors = []struct{ dx, dy int }{
 	{1, -1}, {1, 0}, {1, 1},
 }
 
-func (p Position) ForEachNeighbor(fn func(pos Position)) {
-	for _, neighbor := range neighbors {
-		neighbor := p.offset(neighbor.dx, neighbor.dy)
-		fn(neighbor)
-	}
-}
-
 func (p Position) offset(dx, dy int) Position {
 	return Position{p.X + dx, p.Y + dy}
+}
+
+func (p Position) Neighbors() []Position {
+	nps := make([]Position, 8)
+	for i, nb := range neighbors {
+		nps[i] = p.offset(nb.dx, nb.dy)
+	}
+	return nps
 }
